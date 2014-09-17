861218c2d4730c176bad6312e391add2714939e2
Thomas Foster <thomas.foster80@gmail.com>
2014-09-16 20:50:51 -0400

    removed the need to add username and password in file

--- aws-fog.rb
+++ aws-fog.rb
@@ -2,17 +2,15 @@ require 'rubygems'
 require 'fog'
 
 connection = Fog::Compute.new({
-  :provider                => 'AWS',
-  :aws_access_key_id       => '',
-  :aws_secret_access_key   => '' 
+  :provider                => 'AWS'
   })
 
 server_obj = connection.servers.create(flavor_id: 't1.micro', image_id: 'ami-7c807d14')
 server_obj.wait_for { ready? }
 puts "#{server_obj.id}"
 
-connection.tags.create(resource_id: server_obj.id, key: 'Name', value: 'thomas_build')
-connection.tags.create(resource_id: server_obj.id, key: 'environment', value: 'testing')
+connection.tags.create(resource_id: server_obj.id, key: 'Name', value: 'foster')
+connection.tags.create(resource_id: server_obj.id, key: 'environment', value: 'devops')
 mytags = connection.tags.get('environment')
 puts "#{mytags}"
 
