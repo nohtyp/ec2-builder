385e5bfe5384d54f8ac23572667ad0b728845d4a
Thomas Foster <thomas.foster80@gmail.com>
2014-09-17 21:59:37 -0400

    modified aws-fog to build servers and add security groups while in a vpc

--- aws-fog.rb
+++ aws-fog.rb
@@ -5,11 +5,11 @@ connection = Fog::Compute.new({
   :provider                => 'AWS'
   })
 
-server_obj = connection.servers.create(flavor_id: 't1.micro', image_id: 'ami-7c807d14')
+server_obj = connection.servers.create(flavor_id: 't1.micro', image_id: 'ami-7c807d14',key_name: 'thomas', security_group_ids:["sg-8bbf92ee"], subnet_id: 'subnet-de6b9987')
 server_obj.wait_for { ready? }
 puts "#{server_obj.id}"
 
-connection.tags.create(resource_id: server_obj.id, key: 'Name', value: 'foster')
+connection.tags.create(resource_id: server_obj.id, key: 'Name', value: 'automate_build4')
 connection.tags.create(resource_id: server_obj.id, key: 'environment', value: 'devops')
 mytags = connection.tags.get('environment')
 puts "#{mytags}"
